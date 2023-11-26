99704a3fb2aca28eec337ee7b29c1035636341b4
CharlyWells <wellingtoncharlottenaaodarley@gmail.com>
2023-11-26 11:55:54 +0000

    Added new files

--- 5-sign.c
+++ 5-sign.c
@@ -33,17 +33,21 @@ int main(void)
     return (0);
 }
 
-int print_sign(int n){
-if(n>0){
-  putchar('+');
-  return (1);
+int print_sign(int n)
+{
+if (n > 0)
+{
+putchar('+');
+return (1);
 }
-  else if(n == 0){
-    putchar('0');	  
-    return (0);
-  }
-else {
-   putchar('-');
-   return (-1);
-  }
+else if(n == 0)
+{
+putchar('0');	  
+return (0);
+}
+else
+{
+putchar('-');
+return (-1);
+}
 }
